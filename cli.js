5ef68fa506544c39fa1d77bc88d884990b234a38
Rob Chartier <rob.chartier@gmail.com>
2020-04-16 11:01:18 -0700

    removed some uncessary foul language, added express to host a basig JSON endpoint

--- cli.js
+++ cli.js
@@ -8,6 +8,7 @@ var conf = require('rc')('surly2', {
     version: false
 });
 const debug = require('debug')('surly2');
+process.stdout.write(JSON.stringify(conf));
 
 var options = {
     brain: conf.b || conf.brain || __dirname + '/data/aiml',
@@ -38,11 +39,27 @@ var bot = new Surly({
 console.log('Surly: Hello! Type quit to quit or /help for unhelpful help.');
 process.stdout.write(prompt);
 
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+app.get("/chat", (req, res, next) => {
+    var input = req.query.input;
+
+    bot.talk(input, function (err, response) {
+      res.json({"input" : input, "response" : response, "ts" : (new Date())});
+    });
+ });
+
+ //warm it up...
+ bot.talk("hello world", function (err, response) {
+});
 process.stdin.addListener('data', function (d) {
 	var sentence = d.toString().substring(0, d.length - 1);
 
 	if (sentence === 'quit' || sentence === 'exit') {
-		console.log('Yeah, fuck off.');
+		console.log('Bye.');
 		process.exit(0);
 	}
 
